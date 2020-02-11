43b83eb2d9f503ece64d99b4b08d026d3eadff34
Idong Essien <idong.essien@comcast.net>
2020-02-11 18:07:27 -0500

    header image scales on dblclick, returns initial size on mouseout

--- js/index.js
+++ js/index.js
@@ -1,1 +1,16 @@
 // Your code goes here
+
+
+// DBL CLICK
+
+let imgZoom = document.querySelector('img ');
+
+imgZoom.addEventListener('dblclick', function () {
+        imgZoom.style.transform = "scale(1.3)";
+        imgZoom.style.transition = "transform 1s";
+    });
+
+    imgZoom.addEventListener('mouseout', function () {
+        imgZoom.style.transform = "scale(1)";
+        imgZoom.style.transition = "transform 1s";
+    });
